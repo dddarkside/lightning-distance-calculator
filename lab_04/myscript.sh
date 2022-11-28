ac64b4321d9b2fbd6114f8b17a6276cce44f52a4
Dan <aidarov.danila@gmail.com>
2022-11-29 01:09:27 +0300

    Full update for 4th lab. Not tested!!!

---
+++ lab_04/myscript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mkfifo light_data
+mkfifo sound_data
+./sound_detect_ADN -q sound_data
+./light_detect_ADN -q light_data
+while :
+do
+line=$(head -n 1 light_data)
+if [ $line -ge 13000 ]
+then
+  break
+fi
+done
+start=$SECONDS
+line=$(head -n 1 sound_data)
+line=$(head -n 1 sound_data)
+duration=$(( SECONDS - start ))
+echo $((duration * 343))
